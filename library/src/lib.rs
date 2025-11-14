032d6f919c861ec0c8a61755a82db5e47d80e632
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-11-14 22:15:07 +0000

    leap year kata

--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn is_leap(year: i32) -> bool{
+    let result: bool;
+
+    if is_divisible_by_400(year) {
+        result = true;
+    } else if is_divisible_by_100(year) && !is_divisible_by_400(year) {
+        result = false;
+    } else if is_divisible_by_4(year){
+        result = true;
+    } else {
+        result = false;
+    }
+    println!("{}", result);
+    return result;
 }
+
+pub fn is_divisible_by_4(year: i32) -> bool {
+    let result_divided_by_four = year % 4;
+    return result_divided_by_four == 0;
+}
+
+pub fn is_divisible_by_100(year: i32) -> bool {
+    let result_divided_by_100 = year % 100;
+    return result_divided_by_100 == 0;
+}
+
+pub fn is_divisible_by_400(year: i32) -> bool {
+    let result_divided_by_400 = year % 400;
+    return result_divided_by_400 == 0;
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn not_divisible_by_four() {
+        let result = is_leap(1997);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn divisible_by_four() {
+        let result = is_leap(1996);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn divisible_by_400() {
+        let result = is_leap(1600);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn divisible_by_100_and_not_by_400() {
+        let result = is_leap(1800);
+        assert_eq!(result, false);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-use library::add;
+use library::is_leap;
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+
+    // leap year kata
+    println!("Check to see if a specific year is a leap year, what year would you like to check?");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    is_leap(input.trim().parse().expect("Please enter a valid number"));
+
 }
