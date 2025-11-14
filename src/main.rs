fd978a496b04a82144e6cdec8e5260473091df8b
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-11-14 22:33:12 +0000

    Added input validation

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,20 @@ fn main() {
     // leap year kata
     println!("Check to see if a specific year is a leap year, what year would you like to check?");
 
-    let mut input = String::new();
+    let year = loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+        match input.trim().parse::<i32>() {
+            Ok(num) => break num,
+            Err(_) => {
+                println!("Invalid input. Please enter a valid number:");
+                continue;
+            }
+        }
+    };
 
-    is_leap(input.trim().parse().expect("Please enter a valid number"));
+
+    is_leap(year);
 
 }
